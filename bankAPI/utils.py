cf5635bc4b135c11d3d66b27c1e1ad9e5ab1ee25
Abuton <alaroabubakarolayemi@yahoo.com>
2021-11-28 20:09:48 +0100

    utility script added

---
+++ bankAPI/utils.py
@@ -0,0 +1,8 @@
+from random import randint
+
+
+# function to generate random numbers
+def random_with_N_digits(n):
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
+    return randint(range_start, range_end)
