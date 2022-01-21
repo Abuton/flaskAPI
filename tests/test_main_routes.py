7d02119645fc9c10364e2bc3892f3a313da5670f
Abuton <alaroabubakarolayemi@yahoo.com>
2022-01-21 21:31:56 +0100

    added more test functions

--- tests/test_main_routes.py
+++ tests/test_main_routes.py
@@ -1,5 +1,7 @@
 import unittest
 import requests
+import json
+from pprint import pprint
 from base import BaseTestCase
 
 
@@ -7,25 +9,48 @@ class BackAPITest(BaseTestCase):
     # Ensure that retrievebalance route works as expected
     def test_retrievebalance(self):
         response = requests.get(self.URL+"/retrievebalance")
-        # data = response.json()
+        data = response.json()
 
-        self.assertEqual(response.status_code, 404)
-        # self.assertIn(b'balance', data.keys())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', data.keys())
         # self.assertGreater(data['balance'] > 0)
 
     # Ensure transferhistory route works as expected
-    def test_transferhistory(self, acc_id=2341231256):
-        response = requests.get(self.URL+f"/transferhistory/{int(acc_id)}")
-        # data = response.json()
+    def test_transferhistory(self):
+        response = requests.get(self.URL+f"/transferhistory")
+        data = response.json()
 
-        self.assertEqual(response.status_code, 500)
-        # self.assertNotEqual(len(data), 0)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(len(data), 0)
 
     # Ensure addaccount route works as expected
-    def test_addaccount(self):
-        response = requests.get(self.URL+'/addaccount')
-        statuscode = response.status_code
-        self.assertEqual(statuscode, 500)
+    def test_addaccount_less_than_default(self):
+        params = {
+            "cust_ssn_id": "4321",
+            "acc_type": "test",
+            "amount": "56"
+        }
+
+        response = requests.post(self.URL + '/addaccount', data=params)
+        response = response.content.decode("utf-8")
+        response = json.loads(response)
+        self.assertEqual(response["status_code"], 403)
+        self.assertEqual(response["success"], False)
+        self.assertIn("$100", response["message"])
+
+    def test_addaccount_success(self):
+        params = {
+            "cust_ssn_id": "1234",
+            "acc_type": "test",
+            "amount": "560"
+        }
+
+        response = requests.post(self.URL + '/addaccount', data=params)
+        response = response.content.decode("utf-8")
+        response = json.loads(response)
+        pprint(response)
+        self.assertEqual(response["status_code"], 200)
+        self.assertIn("created", response["message"])
 
 
 if __name__ == "__main__":
