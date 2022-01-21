f9ea26a66d98e3aa9f87f164d2857f151ba3a08a
Abuton <alaroabubakarolayemi@yahoo.com>
2022-01-21 18:47:54 +0100

    removed pipenv dependency

--- startapp.sh
+++ startapp.sh
@@ -2,7 +2,7 @@
 export FLASK_APP=bankAPI
 export FLASK_ENV=dev
 export FLASK_DEBUG=True
-source $(pipenv --venv)/bin/activate
+# source $(pipenv --venv)/bin/activate
 python bankAPI/model/database.py
 python bankAPI/load_data.py
 flask run -h 0.0.0.0
