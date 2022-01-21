d9117bc513abfb934b78b96d364a2fe148e8ae62
Abuton <alaroabubakarolayemi@yahoo.com>
2022-01-21 18:12:19 +0100

    added startapp bash file

---
+++ startapp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export FLASK_APP=bankAPI
+export FLASK_ENV=dev
+export FLASK_DEBUG=True
+source $(pipenv --venv)/bin/activate
+python bankAPI/model/database.py
+python bankAPI/load_data.py
+flask run -h 0.0.0.0
